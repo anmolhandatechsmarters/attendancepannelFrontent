62703d1b22b64fc31a0a457a5aca7755cacd0949
anmolhanda <anmolhanda@techsmarters@gmail.com>
2025-05-22 12:25:31 +0530

    update

---
+++ src/Mytemppage/Unauthorized.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2'; 
+import './Css/unauthorized.css'; 
+
+const UnauthorizedPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const removelocalsystem = async () => {
+      await Swal.fire({
+        title: 'Unauthorized Access',
+        text: 'You have been logged out due to unauthorized access.',
+        icon: 'warning',
+        confirmButtonText: 'Okay'
+      });
+
+      localStorage.removeItem("token");
+      localStorage.removeItem("id");
+      localStorage.removeItem("role");
+      navigate("/login");
+    };
+
+    removelocalsystem();
+  }, [navigate]);
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default UnauthorizedPage;
